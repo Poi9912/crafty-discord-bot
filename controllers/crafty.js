bd46ee9faf5f95f306d55eab76e0675159d82424
Julio Urrego <jut9912@gmail.com>
2026-04-23 23:22:25 -0500

    crafty API control

--- controllers/crafty.js
+++ controllers/crafty.js
@@ -14,63 +14,63 @@ const SERVER_ID = process.env.CRAFTY_MC_INSTANCE;
 console.log('Crafty server instance:', SERVER_ID);
 
 const crafty = axios.create({
-    baseURL: CRAFTY_API_URL,
-    headers: {'Authorization':`Bearer ${API_KEY}`},
-    httpsAgent: httpsAgent
+  baseURL: CRAFTY_API_URL,
+  headers: {'Authorization':`Bearer ${API_KEY}`},
+  httpsAgent: httpsAgent
 });
 
 //get server status from crafty
 async function getServerStatus() {
-    const response = await crafty.get(`/api/v2/servers/${SERVER_ID}/stats`);
-    const stats = response.data.data;
-    const cleanStringPlayers = stats.players.replace(/'/g, '"');
-    const playerListResponse = JSON.parse(cleanStringPlayers) || [];
-    const description = stats.desc.replace(/§./g, '');
-    const mtod = description.trim().replace(/\s+/g, ' ');
-    const memoryUsageGB = (stats.mem / 1024 / 1024 / 1024 ).toFixed(2)+"GB";
-    const utcDateStarted = new Date(stats.started.replace(' ', 'T') + 'Z');
-    const msSinceLastBoot = new Date(Date.now() - utcDateStarted.getTime());
-    const timeSinceLastBoot = msSinceLastBoot.toISOString().substr(11, 8);
-    return {
-        online: stats.running,
-        players: `${stats.online}/${stats.max}`,
-        version: stats.version,
-        empty: stats.online!=0? false : true,
-        cpu: stats.cpu,
-        ram: memoryUsageGB,
-        player_list: playerListResponse,
-        MOTD: mtod,
-        crashed: stats.crashed,
-        starting: stats.waiting_start,
-        last_boot: stats.started,
-        uptime: timeSinceLastBoot,
-    };
+  const response = await crafty.get(`/api/v2/servers/${SERVER_ID}/stats`);
+  const stats = response.data.data;
+  const cleanStringPlayers = stats.players.replace(/'/g, '"');
+  const playerListResponse = JSON.parse(cleanStringPlayers) || [];
+  const description = stats.desc.replace(/§./g, '');
+  const mtod = description.trim().replace(/\s+/g, ' ');
+  const memoryUsageGB = (stats.mem / 1024 / 1024 / 1024 ).toFixed(2)+"GB";
+  const utcDateStarted = new Date(stats.started.replace(' ', 'T') + 'Z');
+  const msSinceLastBoot = new Date(Date.now() - utcDateStarted.getTime());
+  const timeSinceLastBoot = msSinceLastBoot.toISOString().substr(11, 8);
+  return {
+    online: stats.running,
+    players: `${stats.online}/${stats.max}`,
+    version: stats.version,
+    empty: stats.online!=0? false : true,
+    cpu: stats.cpu,
+    ram: memoryUsageGB,
+    player_list: playerListResponse,
+    MOTD: mtod,
+    starting: stats.waiting_start,
+    crashed: stats.crashed,
+    last_boot: stats.started,
+    uptime: timeSinceLastBoot,
+  };
 }
 
 //direct command for autorized mc-admin role
 async function sendConsoleCommand(cmd) {
-    await crafty.post(`/api/v2/servers/${SERVER_ID}/stdin`, cmd, {
-        headers: {
-            'Content-Type': 'text/plain'
-        }
-    });
+  await crafty.post(`/api/v2/servers/${SERVER_ID}/stdin`, cmd, {
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  });
 }
 
 //get last 20 lines of server console
 async function getRecentLogs() {
-    const response = await crafty.get(`/api/v2/servers/${SERVER_ID}/logs`);
-    return response.data.data.slice(-20).join('\n');
+  const response = await crafty.get(`/api/v2/servers/${SERVER_ID}/logs`);
+  return response.data.data.slice(-20).join('\n');
 }
 
 //actions to crafty
 //Valid actions: start, stop, restart, kill
 async function serverAction(action) {
-    return await crafty.post(`/api/v2/servers/${SERVER_ID}/action/${action}`);
+  return await crafty.post(`/api/v2/servers/${SERVER_ID}/action/${action}`);
 }
 
 module.exports = {
-    getServerStatus,
-    sendConsoleCommand,
-    getRecentLogs,
-    serverAction
+  getServerStatus,
+  sendConsoleCommand,
+  getRecentLogs,
+  serverAction
 }
--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,11 @@ const { getServerStatus } = require('../controllers/crafty');
 const { updateBotPresence } = require('../utils/precense');
 
 module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
-        updateBotPresence(client);
-        setInterval(() => updateBotPresence(client), 30000);
-    },
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    updateBotPresence(client);
+    setInterval(() => updateBotPresence(client), 30000);
+  },
 };
